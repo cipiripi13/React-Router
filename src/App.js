bce02553c8e8dbf8fa5efb13e857b613493da1db
cipiripi13 <krstickatarina1990@gmail.com>
2023-03-18 11:06:56 +0100

    adding routes in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 
+import { Route, Routes } from 'react-router';
 import './App.css';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { Home } from './components/Home'
 
 function App() {
+  // u app cemo pisati route... npr routa 1 neka renderuje jednu komponentu i slicno
+  //1. sto radimo jeste da sve nase route obavijemo jednom <Routes></Routes> komponentom
+  //2. - uvodimo jos jednu komponentu <Route/> koja ima 2 stvari path-putanju do nase route
+  //i u element napisemo celu tu nasu komponentu koju smo importovali
+  //ovo je prvi put da pisemo celu nasu komponentu u property neke druge komponente
   return (
-   <p>Cao</p>
+
+    <Routes>
+      {/* pisemo celu nasu komponentu u property neke druge komponente */}
+      {/* path sa '/' zapravo kazemo sta ce se prikazati na toj default stranici */}
+      {/* <Route path={'/'} element={<Home />} />  */}
+      <Route path={'/home'} element={<Home />} />
+      <Route path={'/about'} element={<About />} />
+      <Route path={'/contact'} element={<Contact />} />
+
+      
+
+    </Routes>
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // obmotamo ceo  <App /> u browserRouter i tako ce u celoj nasoj app biti dostupni podaci koji se ticu rutera
+  // i na taj nacin mozemo da koristimo router gde hocemo, ovo radimo ovde zato sto je index.js mozaak nase cele aplikacije
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+
+
 );
 
 
